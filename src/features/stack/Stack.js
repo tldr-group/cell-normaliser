61e8394c370677534f3127801d463366831ed2fd
isaacsquires <isaacsquires@me.com>
2023-02-22 17:24:48 +0000

    performance

--- src/features/stack/Stack.js
+++ src/features/stack/Stack.js
@@ -1,83 +1,72 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import styles from './Stack.module.css';
-import {valueReturn} from './stackFunctions'
-import './../../App.css'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import styles from "./Stack.module.css";
+import { valueReturn } from "./stackFunctions";
+import "./../../App.css";
 import {
   selectLowRateCapacity,
   selectMeasuredCapacity,
   selectAvgVoltage,
   setAvgVoltage,
   setLowRateCapacity,
-  setMeasuredCapacity
-} from './stackSlice'
+  setMeasuredCapacity,
+} from "./stackSlice";
 export function Stack() {
-  const MeasuredCapacity = useSelector(selectMeasuredCapacity)
-  const AverageVoltage = useSelector(selectAvgVoltage)
-  const LowRateCapacity = useSelector(selectLowRateCapacity)
+  const MeasuredCapacity = useSelector(selectMeasuredCapacity);
+  const AverageVoltage = useSelector(selectAvgVoltage);
+  const LowRateCapacity = useSelector(selectLowRateCapacity);
   const dispatch = useDispatch();
-
 
   return (
     <div>
-      <div className='container'>
-      <div className="box-12">
-      <div className={styles.electrodeSection}>
-      <div className='box-row'>
-      <div className="box-12">
-      <p className={styles.title}>
-        Stack measurements
-        </p>
-        </div>
-        </div>
-      <div className='box-row'>
-      <div className="box-12">
+      <div className="container">
         <div className="box-12">
-        <p className={styles.title}>
-            Average Voltage / V
-            </p>
-            <input
-            className={styles.button}
-            aria-label="Set avg voltage"
-            onChange={(e) => dispatch(setAvgVoltage(e.target.value))}
-            value={String(valueReturn(AverageVoltage))}
-            >
-            </input>
+          <div className={styles.electrodeSection}>
+            <div className="box-row">
+              <div className="box-12">
+                <p className={styles.title}>Performance measurements</p>
+              </div>
             </div>
-        
-            <div className="box-12">
-        <p className={styles.title}>
-            Low Rate Capacity / mAh
-            </p>
-            <input
-            className={styles.button}
-            aria-label="Set low rate capacity"
-            onChange={(e) => dispatch(setLowRateCapacity(e.target.value))}
-            value={String(valueReturn(LowRateCapacity))}
-            >
-            </input>
-            </div>
+            <div className="box-row">
+              <div className="box-12">
+                <div className="box-12">
+                  <p className={styles.title}>Average Voltage / V</p>
+                  <input
+                    className={styles.button}
+                    aria-label="Set avg voltage"
+                    onChange={(e) => dispatch(setAvgVoltage(e.target.value))}
+                    value={String(valueReturn(AverageVoltage))}
+                  ></input>
+                </div>
 
-            <div className="box-12">
-        <p className={styles.title}>
-            Measured Capacity / mAh
-            </p>
-            <input
-            className={styles.button}
-            aria-label="Set measured capacity"
-            onChange={(e) => dispatch(setMeasuredCapacity(e.target.value))}
-            value={String(valueReturn(MeasuredCapacity))}
-            >
-            </input>
+                <div className="box-12">
+                  <p className={styles.title}>Low Rate Capacity / mAh</p>
+                  <input
+                    className={styles.button}
+                    aria-label="Set low rate capacity"
+                    onChange={(e) =>
+                      dispatch(setLowRateCapacity(e.target.value))
+                    }
+                    value={String(valueReturn(LowRateCapacity))}
+                  ></input>
+                </div>
 
-            
+                <div className="box-12">
+                  <p className={styles.title}>Measured Capacity / mAh</p>
+                  <input
+                    className={styles.button}
+                    aria-label="Set measured capacity"
+                    onChange={(e) =>
+                      dispatch(setMeasuredCapacity(e.target.value))
+                    }
+                    value={String(valueReturn(MeasuredCapacity))}
+                  ></input>
+                </div>
+              </div>
             </div>
-      
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
